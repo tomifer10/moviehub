3efe2a22b23d6d688d3876c2c13349c3ad6d0f65
tomifer10 <fernandeztomasig@gmail.com>
2024-04-15 15:22:11 +0200

    postgreSql

--- src/controllers/genre.controllers.ts
+++ src/controllers/genre.controllers.ts
@@ -3,10 +3,10 @@ import prisma from "../db/client";
 
 export const createGenre = async (req: Request, res: Response) => {
   const { name } = req.body;
-  const { movieId } = req.params;
+  const movieId = parseInt(req.params.movieIdId);
   try {
-    const genre = await prisma.genres.create({
-      data: { name, movies: { connect: { id: movieId } } },
+    const genre = await prisma.genre.create({
+      data: { name },
     });
     res.status(201).send(genre);
   } catch (error) {
@@ -15,7 +15,7 @@ export const createGenre = async (req: Request, res: Response) => {
 };
 export const getAllGenres = async (req: Request, res: Response) => {
   try {
-    const allGenres = await prisma.genres.findMany();
+    const allGenres = await prisma.genre.findMany();
     res.status(201).send(allGenres);
   } catch (error) {
     res.status(400).send(error);
@@ -24,10 +24,10 @@ export const getAllGenres = async (req: Request, res: Response) => {
 
 export const updateGenre = async (req: Request, res: Response) => {
   const { name } = req.body;
-  const { genreId } = req.params;
+  const genreId = parseInt(req.params.genreId);
 
   try {
-    const genreUpdated = await prisma.genres.update({
+    const genreUpdated = await prisma.genre.update({
       where: { id: genreId },
       data: { name },
     });
@@ -39,9 +39,9 @@ export const updateGenre = async (req: Request, res: Response) => {
 };
 
 export const deleteGenre = async (req: Request, res: Response) => {
-  const { genreId } = req.params;
+  const genreId = parseInt(req.params.genreId);
   try {
-    const genreDeleted = await prisma.genres.delete({
+    const genreDeleted = await prisma.genre.delete({
       where: { id: genreId },
     });
     res.status(200).send(genreDeleted);
